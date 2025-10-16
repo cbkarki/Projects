332c7e90d20e15a74624020ca37e883ca0ce7e44
Chitra Karki <cbkarki@miners.utep.edu>
2025-10-15 21:40:28 -0600

    Updated shiny app, the second plot of the third tag, gender changed to factor.

--- shiny_apps/utep_food_pantry_app/shiny_dashboard.R
+++ shiny_apps/utep_food_pantry_app/shiny_dashboard.R
@@ -6,7 +6,7 @@ dat = read.csv(file = "master.csv",header = T)
 
 library(shiny)
 library("tidyverse")
-library(highcharter) 
+library(highcharter)
 library("billboarder")
 library(ggpubr)
 library(webr)
@@ -14,7 +14,7 @@ library(plotly)
 
 
 ui = fluidPage(
-    
+
     titlePanel(strong("Exploring Food Pantery Data, UTEP")),
        navlistPanel(
                 "Effects of Food Security on Students",
@@ -24,7 +24,7 @@ ui = fluidPage(
                  h4(strong("How is use of government federal aid/assistance associated with
                    food insecurity as measured by the USDA index or categories?")),
                  br(),
-                 p("Below are the bar graphs and the donuts charts to demostrate the 
+                 p("Below are the bar graphs and the donuts charts to demostrate the
                    different levels of food insecurity among the students receiving various
                    Federal Aids in the year 2022.The graphs are interactive and can be changed
                    to different year from the horizontal year bar."),
@@ -36,8 +36,8 @@ ui = fluidPage(
                   study group, there are more number of students with marginal food insecurity.
                    "),
                 br(),
-                 sliderInput("year", label = "Years", 
-                             min = 2019, 
+                 sliderInput("year", label = "Years",
+                             min = 2019,
                              max = 2022,value = 2022 ),
                 h4(strong("Federal Aid Vs Food Insecurity Among Students")),
                 br(),
@@ -46,51 +46,51 @@ ui = fluidPage(
                              highchartOutput("plotbar"),
                              #plotOutput("piedonut")
                             billboarderOutput("bbdonut")
-                             
+
                  )
-                
-        ), 
-        
+
+        ),
+
                tabPanel("Research Question 2",
-                        
+
                  h4(strong(" Does food insecurity (as measured by USDA index or categories)
-                    have a relationship with the items pertaining to concentration on 
+                    have a relationship with the items pertaining to concentration on
                     school and degree progress/completion?")),
                  br(),
-                 p("Graphs below shows the relations between food insecurity, students concentration and 
+                 p("Graphs below shows the relations between food insecurity, students concentration and
                    the progress towards degree completion."),
                  p("There are 152 students who have delayed graduation by 2 semester or more and 144 students
-                 delayed by 1 semester. No mater, students 
+                 delayed by 1 semester. No mater, students
                    belonging to low,marginal or very low food insecurity, think of delaying the graduation and they do
-                   delay. Majority of the students who delayed think almost every day of delaying belong to the 
+                   delay. Majority of the students who delayed think almost every day of delaying belong to the
                    very low food insecurity group.
-                   It is intersting to observe that even  the students who never think of delaying 
+                   It is intersting to observe that even  the students who never think of delaying
                    are delaying their graduation. There could other reason not associated with the food insecurity.
                    "),
                  br(),
-                 
+
                  h4(strong("Food Insecurity  Vs Concentration Vs Degree Completion among Students")),
-                 
+
                  splitLayout(cellWidths = c("60%","40%"),
                              highchartOutput("sankey2"),
                              highchartOutput("donut2")
-                             
+
                              )
-                 
+
         ),
-      
+
         tabPanel("Research Question 3",
-                 h4(strong("Are there gender or ethnicity differences in the items pertaining to 
+                 h4(strong("Are there gender or ethnicity differences in the items pertaining to
                         concentration on school and degree progress/completion?")),
                  br(),
-                 p("Graphs below show the relationship between Gender, students concentration 
+                 p("Graphs below show the relationship between Gender, students concentration
                    and the progress towards degree completion"),
                  p("The number of female students who are delaying graduation are high in number
                    in comparision to other genders. Females who thing almost every day about delaying
                  and delayed in actual are a bit higher in comparision to others. "),
-                 
+
                  br(),
-                 
+
                  h4(strong("Gender Vs Concentration Vs Degree Completion among Students")),
                  splitLayout(cellWidths = c("50%","50%"),
                              highchartOutput("sankey3"),
@@ -105,30 +105,30 @@ ui = fluidPage(
 
 server = function(input, output) {
     output$plotbar = renderHighchart({
-        dat %>% 
-            filter(Year==input$year) %>% 
-            select(FedAid,index) %>% 
-            drop_na() %>%  
-            filter(FedAid != "UTEP's COVID CARES Act Fund" & FedAid != "Other") %>% 
-            group_by(FedAid) %>% 
+        dat %>%
+            filter(Year==input$year) %>%
+            select(FedAid,index) %>%
+            drop_na() %>%
+            filter(FedAid != "UTEP's COVID CARES Act Fund" & FedAid != "Other") %>%
+            group_by(FedAid) %>%
             mutate(Index = ifelse(index==0|index==1,"Mariginal",
-                                  ifelse(index==5|index==6,"Very Low","Low"))) %>%  
-            select(FedAid,Index) %>% 
-            table()  %>%   
-            as.data.frame() %>% arrange(desc(Freq)) %>% 
-            hchart('column', hcaes(x = 'FedAid', y = 'Freq', group = 'Index')) %>%  
+                                  ifelse(index==5|index==6,"Very Low","Low"))) %>%
+            select(FedAid,Index) %>%
+            table()  %>%
+            as.data.frame() %>% arrange(desc(Freq)) %>%
+            hchart('column', hcaes(x = 'FedAid', y = 'Freq', group = 'Index')) %>%
             hc_legend(
                 align = "left",
                 verticalAlign = "top",
                 layout = "horizontal"
-            ) %>% 
+            ) %>%
             hc_xAxis(title = list(text="Federal Aid")) %>% hc_yAxis(title = list(text="#. of Students")) #%>%
            # hc_title(text = paste("Food Security levels in students with different Federal Aids (Year:",input$year,")"))
-        
-        
-        
+
+
+
     })
-    
+
      # output$piedonut = renderPlot({
      #     dat %>%
      #         filter(Year==input$year) %>%
@@ -140,10 +140,10 @@ server = function(input, output) {
      #         PieDonut(aes(FedAid, Index, count=n), title = "",
      #                  ratioByGroup = F,
      #                  #explode = c(5) ,  explodeDonut=TRUE
-     # 
+     #
      #         )
      # })
-     
+
     output$bbdonut = renderBillboarder({
         dat %>%
             filter(Year==input$year) %>%
@@ -159,91 +159,92 @@ server = function(input, output) {
             bb_donutchart(data = donut,title=paste("Federal Aids",input$year)) %>%
             bb_legend(show = TRUE)
     })
-    
+
     output$sankey2 = renderHighchart({
-        inner_join(q1,q2,"RespondentId") %>% 
-            select(-RespondentId) %>% 
+        inner_join(q1,q2,"RespondentId") %>%
+            select(-RespondentId) %>%
             drop_na() %>%
             mutate(Index = ifelse(index==0|index==1,"Mariginal",
-                                  ifelse(index==5|index==6,"Very Low","Low"))) %>% 
+                                  ifelse(index==5|index==6,"Very Low","Low"))) %>%
             select(Index,DiffConcentrate,DelayComplDegree)-> sankey2
-        #group_by(Index,DiffConcentrate,DelayComplDegree)  
-        
-        
+        #group_by(Index,DiffConcentrate,DelayComplDegree)
+
+
         hchart(data_to_sankey(sankey2),type = "sankey")
     })
-    
+
     output$donut2 = renderHighchart({
-        q1 =   dat %>% 
-            filter(Year=="2022") %>% 
+        q1 =   dat %>%
+            filter(Year=="2022") %>%
             select(FedAid,index,RespondentId,Gender)
         dat2 = read.csv(file = "extra_questions_withID.csv",header = T)
-        q2 = dat2 %>% 
+        q2 = dat2 %>%
             select(RespondentId,DiffConcentrate,DelayComplDegree)
-        
-        
-        inner_join(q1,q2,"RespondentId") %>% 
-            select(-RespondentId) %>% 
+
+
+        inner_join(q1,q2,"RespondentId") %>%
+            select(-RespondentId) %>%
             drop_na() %>%
             mutate(Index = ifelse(index==0|index==1,"Mariginal",
-                                  ifelse(index==5|index==6,"Very Low","Low"))) %>% 
-            select(Index,DiffConcentrate,DelayComplDegree) %>% 
-            group_by(Index,DiffConcentrate,DelayComplDegree) %>% 
+                                  ifelse(index==5|index==6,"Very Low","Low"))) %>%
+            select(Index,DiffConcentrate,DelayComplDegree) %>%
+            group_by(Index,DiffConcentrate,DelayComplDegree) %>%
             summarise(n=n()) -> piedo2
-        
+
         #PieDonut(data = piedo2,aes(Index,DiffConcentrate, count=n), title = "hello",ratioByGroup = T,)
-        
-        dout2 = data_to_hierarchical(piedo2,c(Index,DiffConcentrate,DelayComplDegree),n) 
-        
+
+        dout2 = data_to_hierarchical(piedo2,c(Index,DiffConcentrate,DelayComplDegree),n)
+
         hchart(dout2,type="sunburst")
     })
-    q1 =   dat %>% 
-        filter(Year=="2022") %>% 
+    q1 =   dat %>%
+        filter(Year=="2022") %>%
         select(FedAid,index,RespondentId,Gender)
     dat2 = read.csv(file = "extra_questions_withID.csv",header = T)
-    q2 = dat2 %>% 
+    q2 = dat2 %>%
         select(RespondentId,DiffConcentrate,DelayComplDegree)
-    
+
     output$sankey3 = renderHighchart({
-        q3 = dat2 %>% 
+        q3 = dat2 %>%
             select(RespondentId,DiffConcentrate)
         gend = c("Female","Male","transgender","Gender variant","other","Prefere not to say")
-        
+
         inner_join(q1,q2,"RespondentId") %>%
-            #select(FedAid,Gender,DiffConcentrate) %>% 
-            filter(FedAid != "UTEP's COVID CARES Act Fund" & FedAid != "Other") %>% 
-            mutate(Index = ifelse(index==0|index==1,"Mariginal",ifelse(index==5|index==6,"Very Low","Low"))) %>% rename(Gen=Gender) %>% 
-            mutate(Gender = gend[as.numeric(Gen)] ) %>% 
+            #select(FedAid,Gender,DiffConcentrate) %>%
+            filter(FedAid != "UTEP's COVID CARES Act Fund" & FedAid != "Other") %>%
+            mutate(Index = ifelse(index==0|index==1,"Mariginal",ifelse(index==5|index==6,"Very Low","Low"))) %>% rename(Gen=Gender) %>%
+            mutate(Gender = gend[as.numeric(Gen)] ) %>%
             select(Gender,DiffConcentrate,DelayComplDegree) -> sankey3
-        
+
         hchart(data_to_sankey(sankey3),type = "sankey")
-        
+
     })
-    
+
     output$ggbar3 = renderPlot({
         inner_join(q1,q2,"RespondentId") %>%
-            #select(FedAid,Gender,DiffConcentrate) %>% 
-            filter(FedAid != "UTEP's COVID CARES Act Fund" & FedAid != "Other") %>% 
-            mutate(Index = ifelse(index==0|index==1,"Mariginal",ifelse(index==5|index==6,"Very Low","Low"))) %>% 
-            rename(Gen=Gender) %>% 
-            mutate(Gender = as.factor(gend[as.numeric(Gen)] )) %>% 
+            #select(FedAid,Gender,DiffConcentrate) %>%
+            filter(FedAid != "UTEP's COVID CARES Act Fund" & FedAid != "Other") %>%
+            mutate(Index = ifelse(index==0|index==1,"Mariginal",ifelse(index==5|index==6,"Very Low","Low"))) %>%
+            rename(Gen=Gender) %>%
+            mutate(Gender = gend[as.factor(Gen)],
+                   Gender = as.factor(Gender)) %>%
             data.frame() %>% drop_na() -> br3
             p <- ggplot(br3,aes(x=DelayComplDegree,fill=Gender))+
             geom_bar(stat = "count",
                      position = position_dodge( preserve = "single")) +
             facet_grid(DiffConcentrate~.) +
             theme_bw()+
-            theme(legend.position = "top") + 
+            theme(legend.position = "top") +
             ylab("#. of Students")+
-            theme(legend.title= element_blank()) + 
+            theme(legend.title= element_blank()) +
                 scale_fill_hue(l=40)
             p
             #ggplotly(p) %>% config(displayModeBar = F)
-        
+
     })
-    
-  
-    
+
+
+
 }
 
 shinyApp(ui,server)
